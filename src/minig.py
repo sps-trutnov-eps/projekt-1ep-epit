728979319894a09ae87d53b0995dea5dcc8bc0f5
Antonín Vaňouček <vanouceka23@spstrutnov.cz>
2024-04-16 09:23:26 +0200

    piano opraveno

--- src/minig.py
+++ src/minig.py
@@ -1,11 +1,11 @@
 import pygame
 import minigames.minigame_base as mini
 import minigames.test as mini_test
-import minigames.piano 
+import minigames.piano as piano
 # tady odkažte svoji minihru s jejím jménem (stejně jako test)
 minigame_lib = {
     "test": mini_test.test_minigame,
-    "piano": 
+    "piano": piano.piano,
 }
 
 def switch_to_minigame(name, sur: pygame.Surface):
