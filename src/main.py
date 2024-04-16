08a8d827404f769c661ac6529636a6564624230d
Pavel Učený <ucenyp23@spstrutnov.cz>
2024-04-16 09:27:21 +0200

    Async

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import math
-import pygame as pg
+import queue
+from threading import Thread
+import pygame
 from pygame import K_ESCAPE, KEYDOWN, QUIT
 
 SCREEN_WIDTH = 1920
@@ -10,44 +12,54 @@ BLACK = (0, 0, 0)
 
 def handle_events() -> bool:
     """Events handling function."""
-    for event in pg.event.get():
+    for event in pygame.event.get():
         if (event.type == QUIT or
             (event.type == KEYDOWN and event.key == K_ESCAPE)):
             return False
     return True
 
-def map_level(screen: pg.Surface, score: int, seized_land: int) -> int:
+def add_score(clock, score: int, seized_land: int, q: queue.Queue) -> None:
+    dt = clock.get_time() / 1000
+    score += seized_land * dt
+    q.put(math.floor(score))
+
+def map_level(screen: pygame.Surface, seized_land: int) -> int:
     """Level function."""
-    clock = pg.time.Clock()
-    sprites = pg.sprite.Group()
+    clock = pygame.time.Clock()
+    sprites = pygame.sprite.Group()
 
     while handle_events():
-        dt = clock.get_time() / 1000
-        score += seized_land * dt
         sprites.update()
         screen.fill(BLACK)
         sprites.draw(screen)
-        pg.display.update()
+        pygame.display.update()
         clock.tick(60)
+        print(score)
 
-    return score, math.floor(seized_land)
+    return math.floor(seized_land)
 
-def init_game() -> pg.Surface:
+def init_game() -> pygame.Surface:
     """Pygame init function."""
-    pg.init()
-    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pg.display.set_caption('Game')
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('Game')
     return screen
 
 def main() -> None:
     """Main function."""
     screen = init_game()
-    start_time = pg.time.get_ticks()
+    start_time = pygame.time.get_ticks()
+    clock = pygame.time.Clock()
     score = 0
-    seized_land = 0
+    seized_land = 1
+    q = queue.Queue()
 
-    score, seized_land = map_level(screen, score, seized_land)
+    t = Thread(target=add_score, args=(clock, score, seized_land, q))
+    t.start()
+    score = q.get()
+
+    seized_land = map_level(screen, seized_land)
 
 if __name__ == '__main__':
     main()
-    pg.quit()
+    pygame.quit()
