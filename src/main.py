08d4c725ecdd773c4938404c0bc20d469dfd0fb7
ucenyp23 <ucenyp23@spstrutnov.cz>
2024-04-18 21:07:18 +0200

    Fixed things

--- src/main.py
+++ src/main.py
@@ -1,52 +1,48 @@
-import math
 import pygame
 from pygame import K_ESCAPE, KEYDOWN, QUIT
 
-SCREEN_WIDTH = 1920
-SCREEN_HEIGHT = 1080
-
-WHITE = (255, 255, 255)
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 960
 BLACK = (0, 0, 0)
 
-def handle_events() -> bool:
+def handle_events(score: int, seized_land: int) -> int:
     """Events handling function."""
     for event in pygame.event.get():
-        if (event.type == QUIT or
-            (event.type == KEYDOWN and event.key == K_ESCAPE)):
-            return False
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            return None
         if event.type == pygame.USEREVENT:
             score += seized_land
-    return True
+    return score
 
-def map_level(screen: pygame.Surface, seized_land: int, score: int) -> None:
+def update_sprites(sprites: pygame.sprite.Group, screen: pygame.Surface) -> None:
+    sprites.update()
+    screen.fill(BLACK)
+    sprites.draw(screen)
+    pygame.display.update()
+
+def map_level(screen: pygame.Surface, seized_land: int = 0, score: int = 0) -> None:
     """Level function."""
     clock = pygame.time.Clock()
     sprites = pygame.sprite.Group()
 
-    while handle_events():
-        sprites.update()
-        screen.fill(BLACK)
-        sprites.draw(screen)
-        pygame.display.update()
+    while score is not None:
+        score = handle_events(score, seized_land)
+        update_sprites(sprites, screen)
         clock.tick(60)
-        print(score)
 
 def init_game() -> pygame.Surface:
     """Pygame init function."""
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption('Game')
+    pygame.display.set_caption('EPIT')
     return screen
 
 def main() -> None:
     """Main function."""
     screen = init_game()
-    start_time = pygame.time.get_ticks()
     pygame.time.set_timer(pygame.USEREVENT, 1000)
-    score = 0
-    seized_land = 1
 
-    map_level(screen, seized_land, score)
+    map_level(screen)
 
 if __name__ == '__main__':
     main()
