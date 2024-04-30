a72e7d1086625e115a1e6f69f1fe134a673fa3eb
Michal Wolf <wolfm23@spstrutnov.cz>
2024-04-30 08:48:43 +0200

    co dělat ve hře (tutorial)

--- src/minihra klikání.py
+++ src/minihra klikání.py
@@ -13,7 +13,16 @@ okno = pygame.display.set_mode(rozliseni_okna)
 
 antidrzeni = 0
 nahoru = 0
-postup = 10
+postup = rozliseni_okna[0]/2
+IT = 100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+tutorial = 1
+
+
+text_font = pygame.font.SysFont('Arial black', 40)
+
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    okno.blit(img, (x, y))
 
 
 while True:
@@ -21,36 +30,54 @@ while True:
         if udalost.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    
-    stisknute_klavesy = pygame.key.get_pressed()
-    
-
-    
-    
-    if stisknute_klavesy[pygame.K_UP]:
-        nahoru = 1
+    if tutorial == 1:
         
-    if nahoru > 0 and stisknute_klavesy[pygame.K_DOWN]:
-        nahoru = 0
-        postup += 1
+        stisknute_klavesy = pygame.key.get_pressed()
+        okno.fill((255, 255, 255))
+        draw_text('Klikáním na přeskáčku ', text_font, (255,0,0), 10, 50)
+        draw_text('na šipku nahoru a dolu vyhraj', text_font, (255,0,0), 10, 150)
+        draw_text('Dostaň čáru na konec obrazovky', text_font, (255,0,0), 10, 250)
+        draw_text('Hru zapneš stisknutím šipky nahoru nebo dolu ', text_font, (255,0,0), 10, 350)
+        draw_text('Šipky nahoru nebo dolu ', text_font, (255,0,0), 10, 450)
+        pygame.display.flip()
     
-    if stisknute_klavesy[pygame.K_DOWN] and stisknute_klavesy[pygame.K_UP]:
-        postup -= 1
-    
-    
+        pygame.display.update()
+        if stisknute_klavesy[pygame.K_UP] or stisknute_klavesy[pygame.K_DOWN]:
+            tutorial = 0
+    else:
+        
+        stisknute_klavesy = pygame.key.get_pressed()
+          
         
         
-    
-    print(nahoru)
-    print(postup)
-    
-    
-   
-    
-    okno.fill((255, 255, 255))
-    
-    pygame.draw.rect(okno, (0, 0, 0), (rozliseni_okna[1] - rozliseni_okna[1] + rozliseni_okna[1]/4 , 20, postup, 20))
-    
-    
-    pygame.display.update()
-
+        if stisknute_klavesy[pygame.K_UP]:
+            nahoru = 1
+            
+        if nahoru > 0 and stisknute_klavesy[pygame.K_DOWN]:
+            nahoru = 0
+            postup += 10
+        
+        if stisknute_klavesy[pygame.K_DOWN] and stisknute_klavesy[pygame.K_UP]:
+            postup -= 10
+        
+        if IT > 0 :
+            postup -= 0.01
+            
+     
+            
+        
+        
+       
+        
+        okno.fill((255, 255, 255))
+        draw_text('Klikáním na přeskáčku ', text_font, (255,0,0), 10, 50)
+        draw_text('na šipku nahoru a dolu vyhraj', text_font, (255,0,0), 10, 150)
+        draw_text('Dostaň se na konec obrazovky', text_font, (255,0,0), 10, 250)
+        
+        
+        pygame.draw.rect(okno, (0, 0, 0), (rozliseni_okna[1] - rozliseni_okna[1] +1 , 20, postup, 20))
+        
+        pygame.display.flip()
+        
+        pygame.display.update()
+       
