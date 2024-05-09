f1d032480067f200d012c10cb854da701461a927
Ondřej Zbitek <zbiteko23@spstrutnov.cz>
2024-05-09 10:39:41 +0200

    1_minihra

--- src/minigames/nevim zatim.py
+++ src/minigames/nevim zatim.py
@@ -9,14 +9,65 @@ rychlost_hrace = 0.5
 x1 = 615
 y1 = 50
 
+nep1_x = 30
+nep2_x = 1070
+nep3_x = 30
+nep4_x = 970
+nep5_x = 430
+nep6_x = 30
+
+speed1 = 1
+speed2 = 1.5
+speed3 = 2
+speed4 = 2
+speed5 = 1.5
+speed6 = 1
+
 while True:
     for udalost in pygame.event.get():  
         if udalost.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
             
-    stisknute_klavesy = pygame.key.get_pressed() 
- 
+    stisknute_klavesy = pygame.key.get_pressed()
+    
+    nep1_x += speed1
+    nep2_x += speed2
+    nep3_x += speed3
+    nep4_x += speed4
+    nep5_x += speed5
+    nep6_x += speed6
+    
+    if nep1_x > 1150:
+        speed1 *= -1
+    if nep1_x < 0:
+        speed1 *= -1
+        
+    if nep2_x > 1100:
+        speed2 *= -1
+    if nep2_x < 0:
+        speed2 *= -1
+        
+    if nep3_x > 1050:
+        speed3 *= -1
+    if nep3_x < 0:
+        speed3 *= -1
+    
+    if nep4_x > 1000:
+        speed4 *= -1
+    if nep4_x < 0:
+        speed4 *= -1
+        
+    if nep5_x > 850:
+        speed5 *= -1
+    if nep5_x < 0:
+        speed5 *= -1
+        
+    if nep6_x > 950:
+        speed6 *= -1
+    if nep6_x < 0:
+        speed6 *= -1
+        
     if stisknute_klavesy[pygame.K_RIGHT]: 
         x1 += rychlost_hrace
     if stisknute_klavesy[pygame.K_LEFT]: 
@@ -31,9 +82,13 @@ while True:
     hrac = pygame.draw.rect(okno, (255, 0, 255), (x1, y1, 30, 30))
     
     
-        
+    nep1 = pygame.draw.rect(okno, (0, 0, 0), (nep1_x, 150, 100, 50))
+    nep2 = pygame.draw.rect(okno, (0, 0, 0), (nep2_x, 250, 150, 50))
+    nep3 = pygame.draw.rect(okno, (0, 0, 0), (nep3_x, 350, 200, 50))
+    nep4 = pygame.draw.rect(okno, (0, 0, 0), (nep4_x, 450, 250, 50))
+    nep5 = pygame.draw.rect(okno, (0, 0, 0), (nep5_x, 700, 400, 20))
+    nep6 = pygame.draw.rect(okno, (0, 0, 0), (nep6_x, 570, 300, 50))
     
-        
     zed1 = pygame.draw.rect(okno, (255, 0, 0), (0, 0, 1250, 10))
     zed2 = pygame.draw.rect(okno, (255, 0, 0), (1240, 0, 10, 750))
     zed3 = pygame.draw.rect(okno, (255, 0, 0), (0, 0, 10, 750))
@@ -71,6 +126,38 @@ while True:
         zivoty -= 1
         
     if hrac.colliderect(zed5):
+        x1 = 615
+        y1 = 50
+        zivoty -= 1
+    
+#______________________________________
+        
+    if hrac.colliderect(nep1):
+        x1 = 615
+        y1 = 50
+        zivoty -= 1
+    
+    if hrac.colliderect(nep2):
+        x1 = 615
+        y1 = 50
+        zivoty -= 1
+    
+    if hrac.colliderect(nep3):
+        x1 = 615
+        y1 = 50
+        zivoty -= 1
+        
+    if hrac.colliderect(nep4):
+        x1 = 615
+        y1 = 50
+        zivoty -= 1
+    
+    if hrac.colliderect(nep5):
+        x1 = 615
+        y1 = 50
+        zivoty -= 1
+        
+    if hrac.colliderect(nep6):
         x1 = 615
         y1 = 50
         zivoty -= 1
