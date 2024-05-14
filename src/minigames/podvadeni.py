c60f246f47a3c9f8fe54488c497fb41ab257b146
prokopecm23 <prokopecm23@spstrutnov.cz>
2024-05-14 08:35:32 +0200

    funkční časovač

--- src/minigames/podvadeni.py
+++ src/minigames/podvadeni.py
@@ -5,11 +5,13 @@ def podvadeni():
     vyhrál = False
     completion_meter = pygame.Rect(50, 50, 300, 30)
     timer = pygame.Rect(50, 130, 300, 30)
+    time = pygame.Rect(50, 130, 300, 30)
+    seconds = 30000
     
     backdrop = pygame.Rect(0, 0, 1280, 960)
     
     player = pygame.Rect(1100, 460, 100, 100)
-    teacher = pygame.Rect(50, 360, 100, 150)
+    teacher = pygame.Rect(100, 360, 100, 150)
     clock = pygame.time.Clock()
     while True:
         for event in pygame.event.get():
@@ -24,6 +26,13 @@ def podvadeni():
         # stiskni q abys prohrál
         elif pygame.key.get_pressed()[pygame.K_q]:
             return mini.win_minigame()
+        
+        if time.width >= 0:
+            seconds -= 8
+            time.width = seconds/100
+        
+        if time.width <= 0:
+            return mini.fail_minigame()
 
         # zavolej tuto funkci každý frame (kvůli ostatním věcem jako multiplayer)
         mini.mini_frame()
@@ -32,5 +41,6 @@ def podvadeni():
         pygame.draw.rect(mini.mini_surface, (0, 0, 0), player)
         pygame.draw.rect(mini.mini_surface, (255, 0, 0), teacher)
         pygame.draw.rect(mini.mini_surface, (160, 0, 0), completion_meter)
-        pygame.draw.rect(mini.mini_surface, (0, 0, 160), timer)
+        pygame.draw.rect(mini.mini_surface, (0, 0, 70), timer)
+        pygame.draw.rect(mini.mini_surface, (0, 0, 180), time)
         pygame.display.update()
