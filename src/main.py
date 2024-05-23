78d2d9d7f353a60fe5e88ba49b9af9decd35462d
Pavel Učený <ucenyp23@spstrutnov.cz>
2024-05-23 09:58:02 +0200

    Loby hand over

--- src/main.py
+++ src/main.py
@@ -125,6 +125,7 @@ def map_level(screen: pygame.Surface, team: int, score: int = 0) -> None:
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                 return None
+        netcode.send_packet(client_state.server_conn, (land, player_name, protocol_version))
         update_sprites(sprites, screen, team)
         clock.tick(60)
     
